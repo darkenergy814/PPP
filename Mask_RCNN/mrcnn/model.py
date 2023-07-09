d6149a1ee3be0e4cd5d4bd06f51d365f298d745b
DESKTOP-5DKM4TT\JunhoKim <boy814@naver.com>
2023-07-09 16:41:12 +0900

    no message

--- Mask_RCNN/mrcnn/model.py
+++ Mask_RCNN/mrcnn/model.py
@@ -2132,9 +2132,9 @@ class MaskRCNN():
             layers = filter(lambda l: l.name not in exclude, layers)
 
         if by_name:
-            hdf5_format.load_weights_from_hdf5_group(f, layers)
+            hdf5_format.load_weights_from_hdf5_group_by_name(f, layers)
         else:
-            saving.load_weights_from_hdf5_group(f, layers)
+            hdf5_format.load_weights_from_hdf5_group(f, layers)
         if hasattr(f, 'close'):
             f.close()
 
